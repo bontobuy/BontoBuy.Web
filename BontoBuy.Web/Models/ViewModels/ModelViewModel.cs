db58cf8a9808c3fb445340ee0c2a9db13d1f6f92
Patrick Chung <kchungsemchong@gmail.com>
2016-01-26 20:31:29 +0400

    Add Model VM

---
+++ BontoBuy.Web/Models/ViewModels/ModelViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    [Table("Model")]
+    public class ModelViewModel
+    {
+        [Key]
+        public int ModelId { get; set; }
+        public int ItemId { get; set; }
+
+        public ICollection<SpecificationViewModel> ModelSpecificationNav { get; set; }
+        [ForeignKey("ItemId")]
+        public ItemViewModel ModelItemNav { get; set; }
+    }
+}

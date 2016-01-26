07d40e16a516316d92b10523d3346d8562cc4a5d
Patrick Chung <kchungsemchong@gmail.com>
2016-01-26 18:07:04 +0400

    Add Product VM

---
+++ BontoBuy.Web/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    [Table("Product")]
+    public class ProductViewModel
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string Description { get; set; }
+
+        public IEnumerable<ItemViewModel> ProductItemNav { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public CategoryViewModel ProductCategoryNav { get; set; }
+    }
+}

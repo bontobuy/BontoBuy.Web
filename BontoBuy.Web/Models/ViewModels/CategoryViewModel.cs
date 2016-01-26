dc5036e84ac4101c1230e00c1227f71c059d893a
Patrick Chung <kchungsemchong@gmail.com>
2016-01-26 18:06:36 +0400

    Add Category VM

---
+++ BontoBuy.Web/Models/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    [Table("Category")]
+    public class CategoryViewModel
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Description { get; set; }
+
+        public IEnumerable<ProductViewModel> CategoryProductNav { get; set; }
+    }
+}

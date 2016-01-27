9a90f59aede4c58369f2bef9c3b5fbd484e83e4a
Patrick Chung <kchungsemchong@gmail.com>
2016-01-27 10:45:54 +0400

    Add ModelSpecViewModel

---
+++ BontoBuy.Web/Models/ViewModels/ModelSpecViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    public class ModelSpecViewModel
+    {
+        [Key, Column(Order = 0)]
+        public int SpecificationId { get; set; }
+        [Key, Column(Order = 1)]
+        public int ModelId { get; set; }
+        public string Value { get; set; }
+        [ForeignKey("SpecificationId")]
+        public SpecificationViewModel SpecificationNav { get; set; }
+        [ForeignKey("ModelId")]
+        public ModelViewModel ModelNav { get; set; }
+    }
+}

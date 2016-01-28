f904979d00a3cb858f6790f3793b463f30a414b0
Patrick Chung <kchungsemchong@gmail.com>
2016-01-28 18:30:26 +0400

    Add Update, Brand View Model

---
+++ BontoBuy.Web/Models/ViewModels/BrandViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    [Table("Brand")]
+    public class BrandViewModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<ItemViewModel> ItemNav { get; set; }
+    }
+}
---
+++ BontoBuy.Web/Models/ViewModels/Product/UpdateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    public class UpdateViewModel
+    {
+    }
+}

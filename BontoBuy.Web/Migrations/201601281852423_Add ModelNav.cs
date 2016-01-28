e386fac2109630c2a9fa0fe8401395d96d13a1e5
Patrick Chung <kchungsemchong@gmail.com>
2016-01-28 23:04:12 +0400

    Add Migration Add Model Nav

---
+++ BontoBuy.Web/Migrations/201601281852423_Add ModelNav.cs
@@ -0,0 +1,16 @@
+namespace BontoBuy.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddModelNav : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

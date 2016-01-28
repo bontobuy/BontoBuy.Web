207cd8cecb7104f408f0f0f4a173d9c7ad5a2ae0
Patrick Chung <kchungsemchong@gmail.com>
2016-01-28 07:12:56 +0400

    Add Brand Controller

---
+++ BontoBuy.Web/Controllers/BrandController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BontoBuy.Web.Models;
+
+namespace BontoBuy.Web.Controllers
+{
+    public class BrandController : Controller
+    {
+        private readonly IBrandRepo _repository;
+
+        public BrandController(IBrandRepo repo)
+        {
+            _repository = repo;
+        }
+
+        // GET: Brand
+        public ActionResult Retrieve()
+        {
+            try
+            {
+                var records = _repository.Retrieve();
+
+                if (records == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(records);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // GET: Brand/Details/5
+        public ActionResult Get(int id)
+        {
+            try
+            {
+                if (id < 1)
+                {
+                    //return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Product Id cannot be null or empty!");
+                    RedirectToAction("Retrieve");
+                }
+
+                var profile = _repository.Get(id);
+                if (profile == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(profile);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // GET: Brand/Create
+        public ActionResult Create()
+        {
+            try
+            {
+                var newItem = new BrandViewModel();
+
+                return View(newItem);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // POST: Brand/Create
+        [HttpPost]
+        public ActionResult Create(BrandViewModel item)
+        {
+            try
+            {
+                if (item == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Item cannot be null!");
+                }
+
+                var newItem = _repository.Create(item);
+
+                return RedirectToAction("Retrieve");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // GET: Brand/Edit/5
+        public ActionResult Update(int id)
+        {
+            try
+            {
+                BrandViewModel itemToUpdate = new BrandViewModel();
+                if (id < 1)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Invalid Identifier");
+                }
+
+                itemToUpdate = _repository.Get(id);
+                if (itemToUpdate == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(itemToUpdate);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // POST: Brand/Edit/5
+        [HttpPost]
+        public ActionResult Update(int id, BrandViewModel item)
+        {
+            try
+            {
+                if (item == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Product cannot be null");
+                }
+
+                var updatedItem = _repository.Update(id, item);
+                if (updatedItem == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return RedirectToAction("Retrieve");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // GET: Brand/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Brand/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

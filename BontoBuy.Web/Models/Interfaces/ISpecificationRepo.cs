8b62285e6eedcbdfd40e0ffec0446f786b32c220
Patrick Chung <kchungsemchong@gmail.com>
2016-01-29 22:17:39 +0400

    Add interface Tag, Specification

---
+++ BontoBuy.Web/Models/Interfaces/ISpecificationRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    public interface ISpecificationRepo
+    {
+        IEnumerable<SpecificationViewModel> Retrieve();
+        SpecificationViewModel Get(int id);
+        SpecificationViewModel Create(SpecificationViewModel item);
+        SpecificationViewModel Update(int id, SpecificationViewModel item);
+    }
+}
---
+++ BontoBuy.Web/Models/Interfaces/ITagRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BontoBuy.Web.Models
+{
+    public interface ITagRepo
+    {
+        IEnumerable<TagViewModel> Retrieve();
+        TagViewModel Get(int id);
+        TagViewModel Create(TagViewModel item);
+        TagViewModel Update(int id, TagViewModel item);
+    }
+}

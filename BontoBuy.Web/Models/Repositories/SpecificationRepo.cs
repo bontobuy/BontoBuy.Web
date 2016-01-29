b722bf96a879aa2ffc653b2b9b9faf95f4ac0c5a
Patrick Chung <kchungsemchong@gmail.com>
2016-01-29 22:17:55 +0400

    Add Repo Spec, Tag

---
+++ BontoBuy.Web/Models/Repositories/SpecificationRepo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    public class SpecificationRepo : ISpecificationRepo
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IEnumerable<SpecificationViewModel> Retrieve()
+        {
+            var records = db.Specifications.ToList();
+
+            return records;
+        }
+        public SpecificationViewModel Get(int id)
+        {
+            var record = db.Specifications
+               .Where(x => x.SpecificationId == id)
+               .FirstOrDefault();
+
+            return record;
+        }
+        public SpecificationViewModel Create(SpecificationViewModel item)
+        {
+            var newRecord = new SpecificationViewModel
+            {
+                Description = item.Description
+            };
+            db.Specifications.Add(newRecord);
+            db.SaveChanges();
+            return newRecord;
+        }
+
+        public SpecificationViewModel Update(int id, SpecificationViewModel item)
+        {
+            var currentrecord = db.Specifications
+                .Where(x => x.SpecificationId == id)
+                .FirstOrDefault();
+
+            if (!(String.IsNullOrWhiteSpace(item.Description)))
+            {
+                currentrecord.Description = item.Description;
+            }
+            db.SaveChanges();
+
+            return currentrecord;
+        }
+
+        public void Remove(int id)
+        {
+        }
+
+        //public IEnumerable<> GetList()
+        //{
+        //    var records = from r in db.Categories
+        //                  select new { r.CategoryId, r.Description };
+
+        //    return records;
+        //}
+    }
+}
---
+++ BontoBuy.Web/Models/Repositories/TagRepo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    public class TagRepo : ITagRepo
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IEnumerable<TagViewModel> Retrieve()
+        {
+            var records = db.Tags.ToList();
+
+            return records;
+        }
+        public TagViewModel Get(int id)
+        {
+            var record = db.Tags
+               .Where(x => x.TagId == id)
+               .FirstOrDefault();
+
+            return record;
+        }
+        public TagViewModel Create(TagViewModel item)
+        {
+            var newRecord = new TagViewModel
+            {
+                Description = item.Description
+            };
+            db.Tags.Add(newRecord);
+            db.SaveChanges();
+
+            return newRecord;
+        }
+
+        public TagViewModel Update(int id, TagViewModel item)
+        {
+            var currentrecord = db.Tags
+                .Where(x => x.TagId == id)
+                .FirstOrDefault();
+
+            if (!(String.IsNullOrWhiteSpace(item.Description)))
+            {
+                currentrecord.Description = item.Description;
+            }
+            db.SaveChanges();
+
+            return currentrecord;
+        }
+
+        public void Remove(int id)
+        {
+        }
+    }
+}

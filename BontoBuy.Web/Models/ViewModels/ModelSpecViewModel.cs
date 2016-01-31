5aadd2ad8c714d08b58530252b84c36a3780503e
Patrick Chung <kchungsemchong@gmail.com>
2016-01-31 08:38:19 +0400

    Enable Display of Spec Description and Value

--- BontoBuy.Web/Models/ViewModels/ModelSpecViewModel.cs
+++ BontoBuy.Web/Models/ViewModels/ModelSpecViewModel.cs
@@ -12,11 +12,11 @@ namespace BontoBuy.Web.Models
         [Key, Column(Order = 0)]
         [Display(Name = "Specification Id")]
         public int SpecificationId { get; set; }
-        [Key, Column(Order = 1)]
         [Display(Name = "Model Id")]
         public int ModelId { get; set; }
         [Display(Name = "Value")]
         public string Value { get; set; }
+        public string Description { get; set; }
         [ForeignKey("SpecificationId")]
         public SpecificationViewModel SpecificationNav { get; set; }
         [ForeignKey("ModelId")]

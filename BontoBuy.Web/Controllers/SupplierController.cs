6ad5a1bc37e59a23f59b8db03c4f902e331c47f8
Satish Mohun <satishmohun@gmail.com>
2016-02-05 01:40:30 +0400

    Add User Role Check To Supplier

--- BontoBuy.Web/Controllers/SupplierController.cs
+++ BontoBuy.Web/Controllers/SupplierController.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using BontoBuy.Web.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using BontoBuy.Web.Models;
-using Microsoft.AspNet.Identity;
 
 namespace BontoBuy.Web.Controllers
 {
@@ -27,16 +27,13 @@ namespace BontoBuy.Web.Controllers
                 //Check if the "Supplier" role exists if not it returns a null value
                 var role = db.Roles.SingleOrDefault(m => m.Name == "Supplier");
 
-                if (role != null)
+                if (User.IsInRole("Supplier"))
                 {
                     //Runs a query to determine if the user is actually an "Supplier" if not it returns a null value
-                    var userInRole = db.Users.Where(m => m.Roles.Any(r => r.UserId == userId)).FirstOrDefault();
-                    if (userInRole != null)
-                    {
-                        return View();
-                    }
-
-                    return RedirectToAction("LoginSupplier", "Account");
+                    //var userInRole = db.Users.Where(m => m.Roles.Any(r => r.UserId == userId)).FirstOrDefault();
+                    //if (userInRole != null)
+                    //{
+                    return View();
                 }
                 return RedirectToAction("LoginSupplier", "Account");
             }

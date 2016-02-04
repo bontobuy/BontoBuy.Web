43c5dae1e134096ecb07bbb1f06d3cc6daf60b61
Patrick Chung <kchungsemchong@gmail.com>
2016-02-04 09:35:38 +0400

    Add Model Spec Id

--- BontoBuy.Web/Models/ViewModels/ModelSpecViewModel.cs
+++ BontoBuy.Web/Models/ViewModels/ModelSpecViewModel.cs
@@ -9,17 +9,35 @@ namespace BontoBuy.Web.Models
 {
     public class ModelSpecViewModel
     {
-        [Key, Column(Order = 0)]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ModelSpecId { get; set; }
         [Display(Name = "Specification Id")]
+        [Column(Order = 0)]
         public int SpecificationId { get; set; }
         [Display(Name = "Model Id")]
+        [Column(Order = 1)]
         public int ModelId { get; set; }
         [Display(Name = "Value")]
         public string Value { get; set; }
-        public string Description { get; set; }
+
+        //[Column(Order = 2)]
+        //public int PhotoId { get; set; }
         [ForeignKey("SpecificationId")]
         public SpecificationViewModel SpecificationNav { get; set; }
         [ForeignKey("ModelId")]
         public ModelViewModel ModelNav { get; set; }
+
+        //[ForeignKey("PhotoId")]
+        //public PhotoViewModel PhotoNav { get; set; }
+    }
+
+    public class ModelSpecCreationViewModel
+    {
+        public int ModelSpecId { get; set; }
+        public int SpecificationId { get; set; }
+        public int ModelId { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
     }
 }

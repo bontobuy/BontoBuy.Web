8149ba7901279762e0acc5d071a6cf4b7e9a0a0b
Satish Mohun <satishmohun@gmail.com>
2016-02-08 20:48:14 +0400

    Add Class For Retrieving Name

---
+++ BontoBuy.Web/Models/Name.cs
@@ -0,0 +1,23 @@
+﻿using BontoBuy.Web.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BontoBuy.Web.Models
+{
+    public class Name
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public string name { get; set; }
+        public string GetName(string id)
+        {
+            string name = db.Users.Where(x => x.Id == id).Select(x => x.Name).Single();
+            return name;
+        }
+    }
+}

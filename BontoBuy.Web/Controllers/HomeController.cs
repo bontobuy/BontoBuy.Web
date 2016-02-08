65a976e6748c2baceaf171ab1f1364833d7ccd8f
Satish Mohun <satishmohun@gmail.com>
2016-02-08 23:14:48 +0400

    Update Home Sidebar FE with Category, Product, Item

--- BontoBuy.Web/Controllers/HomeController.cs
+++ BontoBuy.Web/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using BontoBuy.Web.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +12,51 @@ namespace BontoBuy.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICategoryRepo _categoryRepository;
+
+        //private readonly IProductRepo _productRepository;
+        //private readonly IItemRepo _itemRepository;
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public HomeController(ICategoryRepo catRepo)
+        {
+            _categoryRepository = catRepo;
+
+            //_productRepository = prodRepo;
+            //_itemRepository = iteRepo;
+        }
         public ActionResult Index()
         {
-            return View();
+            var categoryRecords = _categoryRepository.Retrieve();
+            if (categoryRecords == null)
+            {
+                return HttpNotFound();
+            }
+
+            var productRecords = from prod in db.Products
+                                 join cat in db.Categories on prod.CategoryId equals cat.CategoryId
+                                 where prod.Status == "Active"
+                                 select prod;
+            if (productRecords == null)
+            {
+                return HttpNotFound();
+            }
+
+            var itemRecords = from item in db.Items
+                              join prod in db.Products on item.ProductId equals prod.ProductId
+                              where item.Status == "Active"
+                              select item;
+            if (itemRecords == null)
+            {
+                return HttpNotFound();
+            }
+
+            dynamic model = new ExpandoObject();
+            model.Category = categoryRecords;
+            model.Product = productRecords;
+            model.Item = itemRecords;
+
+            return View(model);
         }
 
         public ActionResult About()

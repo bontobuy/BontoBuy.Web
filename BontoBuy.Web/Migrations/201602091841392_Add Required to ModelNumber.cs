d4b1f3fe863bdfe949a914d99dd47ebfbfa48f8f
Patrick Chung <kchungsemchong@gmail.com>
2016-02-09 22:49:18 +0400

    Migration Add Required to modelNumber

---
+++ BontoBuy.Web/Migrations/201602091841392_Add Required to ModelNumber.cs
@@ -0,0 +1,18 @@
+namespace BontoBuy.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddRequiredtoModelNumber : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Model", "ModelNumber", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Model", "ModelNumber", c => c.String());
+        }
+    }
+}

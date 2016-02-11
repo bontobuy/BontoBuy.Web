90b0992989417476e3e58791408c4665ef7e0813
Patrick Chung <kchungsemchong@gmail.com>
2016-02-11 20:53:28 +0400

    Add PhotoModel

---
+++ BontoBuy.Web/Models/ViewModels/PhotoModelViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    [Table("PhotoModel")]
+    public class PhotoModelViewModel
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int PhotoId { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public int ModelId { get; set; }
+
+        public IEnumerable<PhotoViewModel> PhotoNav { get; set; }
+        public IEnumerable<ModelViewModel> ModelNav { get; set; }
+    }
+}

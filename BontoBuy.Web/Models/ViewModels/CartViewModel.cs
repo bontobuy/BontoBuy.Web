34ef9d15717bbd2fbd18b4c56acb3a4175beebe6
Patrick Chung <kchungsemchong@gmail.com>
2016-02-13 20:15:55 +0400

    Add Cart ViewModel

---
+++ BontoBuy.Web/Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    public class CartViewModel
+    {
+        public int SpecificationId { get; set; }
+        public int SupplierId { get; set; }
+        public int ModelId { get; set; }
+        public string Value { get; set; }
+        public int ModelSpecId { get; set; }
+        public string Description { get; set; }
+        public string UserId { get; set; }
+
+        //  public int CustomerId { get; set; }
+    }
+
+    public class CartDisplayViewModel
+    {
+        public string UserId { get; set; }
+        public string ModelName { get; set; }
+    }
+}

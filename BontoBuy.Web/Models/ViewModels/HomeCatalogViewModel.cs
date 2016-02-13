403d702202e04699bae93ab2de1226712c421cde
Satish Mohun <satishmohun@gmail.com>
2016-02-14 02:21:06 +0400

    Add HomeCatalogVM For Retrieve

---
+++ BontoBuy.Web/Models/ViewModels/HomeCatalogViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    public class HomeCatalogViewModel
+    {
+        public int ModelId { get; set; }
+        public string ModelNumber { get; set; }
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+        public int Price { get; set; }
+        public int SpecificationId { get; set; }
+        public string SpecificationDescription { get; set; }
+        public string SpecificationValue { get; set; }
+        public string ImageUrl { get; set; }
+        public string PhysicalPath { get; set; }
+    }
+}

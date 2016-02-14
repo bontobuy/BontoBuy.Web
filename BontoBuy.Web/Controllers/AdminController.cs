0230600302ffbd96139ffcd5fbf994dd64519d33
Patrick Chung <kchungsemchong@gmail.com>
2016-02-14 07:01:48 +0400

    Add Admin Status View Model

--- BontoBuy.Web/Controllers/AdminController.cs
+++ BontoBuy.Web/Controllers/AdminController.cs
@@ -1,17 +1,19 @@
-﻿using BontoBuy.Web.Models;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BontoBuy.Web.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 
 namespace BontoBuy.Web.Controllers
 {
     public class AdminController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Admin
         public ActionResult Index()
         {
@@ -28,6 +30,23 @@ namespace BontoBuy.Web.Controllers
                 return RedirectToAction("Index", "Home");
             }
             return RedirectToAction("Login", "Account");
+        }
+
+        public ActionResult RetrievePendingModels()
+        {
+            var pendingModels = from m in db.Models
+                                where m.Status == "Pending"
+                                select m;
+
+            return View(pendingModels);
+        }
+
+        public ActionResult EditModelStatus(int id)
+        {
+            int modelId = id;
+
+            ViewBag.StatusId = new SelectList(db.Categories.Where(x => x.Status == "Active"), "CategoryId", "Description");
+            return null;
         }
     }
 }

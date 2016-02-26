705e0bcbea7b5332658f203c51afc2e49a94fd48
Patrick Chung <kchungsemchong@gmail.com>
2016-02-26 21:55:33 +0400

    Add Action For PDF Conversion

--- BontoBuy.Web/Controllers/OrderController.cs
+++ BontoBuy.Web/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Rotativa;
 
 namespace BontoBuy.Web.Controllers
 {
@@ -12,6 +13,11 @@ namespace BontoBuy.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ViewPdf()
+        {
+            return new ViewAsPdf("Invoice");
         }
 
         public ActionResult Invoice()

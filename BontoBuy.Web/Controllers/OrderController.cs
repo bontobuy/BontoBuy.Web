cf4c135154a50ba91a5d3ddbfd046633c26b4e09
Satish Mohun <satishmohun@gmail.com>
2016-02-26 21:37:51 +0400

    Add Controller Order, and Invoice View

---
+++ BontoBuy.Web/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BontoBuy.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Invoice()
+        {
+            return View();
+        }
+    }
+}

45871615f9082d0aca66ceed32c1ba516776d1bf
Satish Mohun <satishmohun@gmail.com>
2016-03-01 00:46:23 +0400

    Add Customer Controller And Methods

---
+++ BontoBuy.Web/Controllers/CustomerController.cs
@@ -0,0 +1,122 @@
+﻿using BontoBuy.Web.Models;
+using Microsoft.AspNet.Identity;
+using Rotativa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BontoBuy.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CustomerRetrieveOrders()
+        {
+            try
+            {
+                string userId = User.Identity.GetUserId();
+                if (userId == null)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+
+                if (User.IsInRole("Customer"))
+                {
+                    var orderList = new List<CustomerRetrieveOrdersViewModel>();
+                    var records = from o in db.Orders
+                                  where o.CustomerUserId == userId
+                                  select o;
+                    foreach (var item in records)
+                    {
+                        var orderItem = new CustomerRetrieveOrdersViewModel()
+                        {
+                            OrderId = item.OrderId,
+                            ModelId = item.ModelId,
+                            ModelNumber = (from m in db.Models
+                                           where m.ModelId == item.ModelId
+                                           select m.ModelNumber).FirstOrDefault(),
+                            SupplierName = (from c in db.Suppliers
+                                            where c.SupplierId == item.SupplierId
+                                            select c.Name).FirstOrDefault(),
+                            Status = item.Status,
+                            DtCreated = item.DtCreated
+                        };
+                        orderList.Add(orderItem);
+                    }
+                    return View(orderList);
+                }
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        public ActionResult CustomerGetOrder(int id)
+        {
+            try
+            {
+                string userId = User.Identity.GetUserId();
+                if (userId == null)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                if (User.IsInRole("Customer"))
+                {
+                    var record = (from o in db.Orders
+                                  where o.OrderId == id
+                                  select o).FirstOrDefault();
+
+                    var customerOrder = new CustomerGetOrderViewModel()
+                    {
+                        OrderId = record.OrderId,
+                        SupplierName = (from c in db.Suppliers
+                                        where c.SupplierId == record.SupplierId
+                                        select c.Name).FirstOrDefault(),
+                        ModelNumber = (from m in db.Models
+                                       where m.ModelId == record.ModelId
+                                       select m.ModelNumber).FirstOrDefault(),
+                        Status = record.Status,
+                        Total = record.Total,
+                        GrandTotal = record.Total,
+                        DtCreated = record.DtCreated,
+                        ExpectedDeliveryDate = record.ExpectedDeliveryDate,
+                        RealDeliveryDate = record.RealDeliveryDate,
+                        CustomerFName = (from u in db.Users
+                                         where u.Id == userId
+                                         select u.FirstName).FirstOrDefault(),
+                        CustomerLName = (from u in db.Users
+                                         where u.Id == userId
+                                         select u.LastName).FirstOrDefault()
+                    };
+                    Session["CustomerOrder"] = customerOrder;
+                    return View(customerOrder);
+                }
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        public ActionResult ViewOrderPdf()
+        {
+            var customerOrder = Session["CustomerOrder"] as CustomerGetOrderViewModel;
+            Session.Remove("CustomerOrder");
+
+            return new ViewAsPdf("ViewOrderPdf", customerOrder);
+        }
+    }
+}

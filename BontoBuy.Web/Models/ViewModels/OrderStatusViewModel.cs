3a798b155a399ff4302b32a293064cf3c18529bb
Patrick Chung <kchungsemchong@gmail.com>
2016-02-29 20:18:56 +0400

    Add OrderStatus VM

---
+++ BontoBuy.Web/Models/ViewModels/OrderStatusViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    public class OrderStatusViewModel
+    {
+        [Key]
+        public int OrderStatusId { get; set; }
+        public string Name { get; set; }
+    }
+}

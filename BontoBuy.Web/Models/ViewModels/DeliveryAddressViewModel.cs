a7aaa96371168f6bf0151fef71460fe8454871f6
Patrick Chung <kchungsemchong@gmail.com>
2016-03-02 22:44:36 +0400

    Add VM  DeliveryAddress

---
+++ BontoBuy.Web/Models/ViewModels/DeliveryAddressViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace BontoBuy.Web.Models
+{
+    [Table("DeliveryAddress")]
+    public class DeliveryAddressViewModel
+    {
+        [Key]
+        public int DeliveryAddressId { get; set; }
+        public string UserId { get; set; }
+        public int CustomerId { get; set; }
+        public string Street { get; set; }
+        public string Zipcode { get; set; }
+
+        [ForeignKey("UserId")]
+        public CustomerViewModel CustomerNav { get; set; }
+    }
+}

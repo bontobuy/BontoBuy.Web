daba88360cbef3d2662f8af364da69e8ec358b0b
Patrick Chung <kchungsemchong@gmail.com>
2016-03-03 21:36:28 +0400

    Add Wishlist VM

---
+++ BontoBuy.Web/Models/ViewModels/WishlistViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    [Table("Wishlist")]
+    public class WishlistViewModel
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int ModelId { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int WishlistId { get; set; }
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<ModelViewModel> ModelNav { get; set; }
+        public IEnumerable<CustomerViewModel> CustomerNav { get; set; }
+    }
+}

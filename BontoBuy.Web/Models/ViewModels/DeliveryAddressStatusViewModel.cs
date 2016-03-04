154916feb3110d155591e2f768929a4932816c1f
Patrick Chung <kchungsemchong@gmail.com>
2016-03-04 14:22:23 +0400

    Add DeliveryStatus ViewModel

---
+++ BontoBuy.Web/Models/ViewModels/DeliveryAddressStatusViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    [Table("DeliveryAddressStatus")]
+    public class DeliveryAddressStatusViewModel
+    {
+        [Key]
+        public int DeliveryAddressStatusId { get; set; }
+        public string Status { get; set; }
+    }
+}

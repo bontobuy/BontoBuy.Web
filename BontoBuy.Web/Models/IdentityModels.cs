e889de2b177d0128b34656e1fa90ce8521839f3c
Patrick Chung <kchungsemchong@gmail.com>
2016-03-04 10:40:40 +0400

    Add Remove Columns from Application User

--- BontoBuy.Web/Models/IdentityModels.cs
+++ BontoBuy.Web/Models/IdentityModels.cs
@@ -11,8 +11,6 @@ namespace BontoBuy.Web.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string Street { get; set; }
-        public string City { get; set; }
         public string Status { get; set; }
         public string Name { get; set; }
         public string FirstName { get; set; }

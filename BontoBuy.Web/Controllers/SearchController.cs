26856124c85b8419a0cd376227bb2d1db3a4f91b
Satish Mohun <satishmohun@gmail.com>
2016-03-06 17:03:21 +0400

    Add Method For AdvancedSearch & SearchResult

---
+++ BontoBuy.Web/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using BontoBuy.Web.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BontoBuy.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Search
+        public ActionResult SearchResult()
+        {
+            return View();
+        }
+
+        public ActionResult AdvancedSearch()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories.Where(x => x.Status == "Active"), "CategoryId", "Description");
+            ViewBag.BrandId = new SelectList(db.Brands.Where(b => b.Status == "Active"), "BrandId", "Name");
+            return View();
+        }
+    }
+}

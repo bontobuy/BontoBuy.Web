c3cb93fcef45b9d1e00c1dc9aa7e92d514c9488d
Patrick Chung <kchungsemchong@gmail.com>
2016-03-06 10:41:39 +0400

    Add Return Status

---
+++ BontoBuy.Web/Models/ViewModels/ReturnStatusViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    [Table("ReturnStatus")]
+    public class ReturnStatusViewModel
+    {
+        [Key]
+        public int ReturnStatusId { get; set; }
+        public string Status { get; set; }
+    }
+}

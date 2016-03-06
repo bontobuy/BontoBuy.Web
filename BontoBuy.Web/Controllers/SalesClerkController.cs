231770e42e58626d1ec7c753a626a197daff88c6
Satish Mohun <satishmohun@gmail.com>
2016-03-06 10:12:31 +0400

    Add Sales Clerk Layout + Controller + Views

---
+++ BontoBuy.Web/Controllers/SalesClerkController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BontoBuy.Web.Controllers
+{
+    public class SalesClerkController : Controller
+    {
+        // GET: SalesClerk
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RetrieveOrders()
+        {
+            return View();
+        }
+    }
+}

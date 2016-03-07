daccf89e6ae1f953a0ca4c7eced1d42f37d7715d
Patrick Chung <kchungsemchong@gmail.com>
2016-03-07 08:48:34 +0400

    Add PaymentStatus VM

---
+++ BontoBuy.Web/Models/ViewModels/PaymentStatusViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    [Table("PaymentStatus")]
+    public class PaymentStatusViewModel
+    {
+        [Key]
+        public int PaymentStatusId { get; set; }
+        public string Status { get; set; }
+    }
+}

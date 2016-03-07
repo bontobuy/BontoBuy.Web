21ba94cc0613e3b4032f08efd6f2167e364831de
Satish Mohun <satishmohun@gmail.com>
2016-03-08 00:05:53 +0400

    Add CustomerReturnsController Including Methods CreateReturn, Retrieve, GetReturn

---
+++ BontoBuy.Web/Controllers/CustomerReturnsController.cs
@@ -0,0 +1,93 @@
+﻿using BontoBuy.Web.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BontoBuy.Web.Controllers
+{
+    public class CustomerReturnsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: CustomerReturns
+        public ActionResult RetrieveReturns()
+        {
+            string userId = User.Identity.GetUserId();
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var records = from r in db.Returns
+                          join o in db.Orders on r.OrderId equals o.OrderId
+                          where o.CustomerUserId == userId
+                          select r;
+            if (records == null)
+                return RedirectToAction("Home", "Error404");
+
+            return View(records);
+        }
+
+        public ActionResult GetReturns(int id)
+        {
+            string userId = User.Identity.GetUserId();
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var record = (from r in db.Returns
+                          join o in db.Orders on r.OrderId equals o.OrderId
+                          where o.CustomerUserId == userId &&
+                          r.ReturnId == id
+                          select r).FirstOrDefault();
+
+            if (record == null)
+                return RedirectToAction("Home", "Error404");
+
+            return View(record);
+        }
+
+        public ActionResult CreateReturn(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            if (userId == null || !User.IsInRole("Customer"))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            var newReturn = new ReturnViewModel();
+            Session["ReturnOrderId"] = id;
+            ViewBag.OrderId = id;
+            return View(newReturn);
+        }
+
+        [HttpPost]
+        public ActionResult CreateReturn(ReturnViewModel item)
+        {
+            string userId = User.Identity.GetUserId();
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            int orderId = Convert.ToInt32(Session["ReturnOrderId"]);
+            var newReturn = new ReturnViewModel()
+            {
+                OrderId = orderId,
+                Reason = item.Reason,
+                Status = "Pending",
+                DtCreated = DateTime.UtcNow,
+                ReturnDate = DateTime.UtcNow,
+                DtUpdated = DateTime.UtcNow
+            };
+            db.Returns.Add(newReturn);
+            db.SaveChanges();
+
+            return RedirectToAction("RetrieveReturns", "CustomerReturns");
+        }
+    }
+}

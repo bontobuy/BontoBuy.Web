ccefd6f217a422af054ba14238bd05dd61f52418
Satish Mohun <satishmohun@gmail.com>
2016-03-09 15:33:39 +0400

    Add AdminRetrieveModel & AdminGetModel in AdminVM

--- BontoBuy.Web/Models/ViewModels/AdminViewModel.cs
+++ BontoBuy.Web/Models/ViewModels/AdminViewModel.cs
@@ -18,4 +18,37 @@ namespace BontoBuy.Web.Models
         public int ModelId { get; set; }
         public string Status { get; set; }
     }
+
+    public class AdminRetrieveOrdersViewModel
+    {
+        public int OrderId { get; set; }
+        public int ModelId { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string ModelNumber { get; set; }
+        public string SupplierName { get; set; }
+        public string Status { get; set; }
+        public DateTime DtCreated { get; set; }
+    }
+
+    public class AdminGetOrderViewModel
+    {
+        public int OrderId { get; set; }
+        public string SupplierName { get; set; }
+        public string ModelNumber { get; set; }
+        public string Status { get; set; }
+        public int Total { get; set; }
+        public int GrandTotal { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DtCreated { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+        public DateTime RealDeliveryDate { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string CustomerFName { get; set; }
+        public string CustomerLName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public int UnitPrice { get; set; }
+    }
 }

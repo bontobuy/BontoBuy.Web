0bf11a3f7912e697b691f8f7ba2406c66034a899
Patrick Chung <kchungsemchong@gmail.com>
2016-03-11 10:02:30 +0400

    Add Interface AdminRepo

---
+++ BontoBuy.Web/Models/Interfaces/IAdminItemRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BontoBuy.Web.Models
+{
+    public interface IAdminItemRepo
+    {
+        IEnumerable<AdminRetrieveItemViewModel> Retrieve();
+        AdminGetItemViewModel Get(int id);
+        ItemViewModel Create(AdminCreateItemViewModel item);
+        ItemViewModel Update(AdminUpdateItemViewModel item);
+        AdminUpdateItemViewModel GetUpdateItemViewModel(int id);
+        IEnumerable<AdminRetrieveItemViewModel> RetrieveArchives();
+        bool Archive(int id);
+        bool RevertArchive(int id);
+
+        IEnumerable<ProductViewModel> AdminRetrieveProducts();
+    }
+}

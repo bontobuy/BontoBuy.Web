1d4bb224bcbd36021f31566508e093820d00c827
Patrick Chung <kchungsemchong@gmail.com>
2016-03-11 10:03:38 +0400

    Edit AdminViewModel

--- BontoBuy.Web/Models/ViewModels/AdminViewModel.cs
+++ BontoBuy.Web/Models/ViewModels/AdminViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,60 @@ namespace BontoBuy.Web.Models
     public class AdminModelStatusViewModel
     {
         public int ModelId { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class AdminRetrieveItemViewModel
+    {
+        public int ItemId { get; set; }
+        public int ProductId { get; set; }
+        [Display(Name = "Product Type")]
+        public string ProductDescription { get; set; }
+        [Display(Name = "Item Name")]
+        public string ItemDescription { get; set; }
+        public string Status { get; set; }
+        [Display(Name = "Date Created")]
+        public DateTime DtCreated { get; set; }
+        [Display(Name = "Date Updated")]
+        public DateTime DtUpdated { get; set; }
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+    }
+
+    public class AdminGetItemViewModel
+    {
+        public int ItemId { get; set; }
+        public int ProductId { get; set; }
+        [Display(Name = "Product Type")]
+        public string ProductDescription { get; set; }
+        [Display(Name = "Item Name")]
+        public string ItemDescription { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class AdminCreateItemViewModel
+    {
+        [Required]
+        [Display(Name = "Product Name")]
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "Item Name")]
+        public string ItemDescription { get; set; }
+        public DateTime DtCreated { get; set; }
+        public DateTime DtUpdated { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class AdminUpdateItemViewModel
+    {
+        public int ItemId { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "Item Name")]
+        public string ItemDescription { get; set; }
+        public DateTime DtUpdated { get; set; }
         public string Status { get; set; }
     }
 }

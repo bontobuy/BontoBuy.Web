0c57ef18266541534e4a10e6a1d7a3a177fd0859
Patrick Chung <kchungsemchong@gmail.com>
2016-03-12 09:23:30 +0400

    Add AdminItemTest

---
+++ BontoBuy.Web.Tests/Controllers/AdminItemTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BontoBuy.Web.Controllers;
+using BontoBuy.Web.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BontoBuy.Web.Tests.Controllers
+{
+    [TestClass]
+    public class AdminItemTest
+    {
+        [TestMethod]
+        public void Retrieve()
+        {
+            var list = new List<AdminRetrieveItemViewModel>();
+            var adminRepoMock = new Mock<IAdminItemRepo>();
+            adminRepoMock.Setup(m => m.Retrieve()).Returns(list);
+            var controller = new AdminItemController(adminRepoMock.Object);
+
+            var result = controller.Retrieve();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}

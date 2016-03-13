cc075e942a3a3c047912aea8cb88f86f5654e79e
Patrick Chung <kchungsemchong@gmail.com>
2016-03-13 21:40:30 +0400

    Add AdminRoleViewModel

--- BontoBuy.Web/Models/ViewModels/RoleViewModel.cs
+++ BontoBuy.Web/Models/ViewModels/RoleViewModel.cs
@@ -10,6 +10,60 @@ namespace BontoBuy.Web.Models
     {
     }
 
+    public class AdminRetrieveUsersViewModel
+    {
+        public string UserId { get; set; }
+        public List<IdentityRole> UserRoles { get; set; }
+        public List<string> CurrentRoles { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class AdminRetrieveCustomersViewModel
+    {
+        public string UserId { get; set; }
+        public List<IdentityRole> UserRoles { get; set; }
+        public List<string> CurrentRoles { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class AdminRetrieveSuppliersViewModel
+    {
+        public string UserId { get; set; }
+        public List<IdentityRole> UserRoles { get; set; }
+        public List<string> CurrentRoles { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class AdminUpdateSupplierRoleViewModel
+    {
+        public string UserId { get; set; }
+        public List<IdentityRole> UserRoles { get; set; }
+        public List<string> CurrentRoles { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class AdminUpdateCustomerRoleViewModel
+    {
+        public string UserId { get; set; }
+        public List<IdentityRole> UserRoles { get; set; }
+        public List<string> CurrentRoles { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class AdminUpdateUserRoleViewModel
+    {
+        public string UserId { get; set; }
+        public List<IdentityRole> UserRoles { get; set; }
+        public List<string> CurrentRoles { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+    }
+
     public class UserRoleViewModel
     {
         public string UserId { get; set; }

f5c8e1f017de6f090de9f8878ff1ef8d0f1a8128
Patrick Chung <kchungsemchong@gmail.com>
2016-03-14 22:33:54 +0400

    Edit config

--- BontoBuy.Web/Global.asax.cs
+++ BontoBuy.Web/Global.asax.cs
@@ -1,11 +1,11 @@
-﻿using BontoBuy.Web.App_Start;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using BontoBuy.Web.App_Start;
 
 namespace BontoBuy.Web
 {
@@ -17,6 +17,9 @@ namespace BontoBuy.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var initData = new InitializeData();
+            initData.InitializeCommissions();
 
             //var initRole = new InitializeRoles();
             //initRole.InitializeAllRoles();

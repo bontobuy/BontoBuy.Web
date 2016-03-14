656a9c92187fe4b959addb53bd87e581c6ff7e04
Patrick Chung <kchungsemchong@gmail.com>
2016-03-14 22:30:12 +0400

    Add AdminCommissionRepo

---
+++ BontoBuy.Web/Models/Repositories/AdminCommissionRepo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    public class AdminCommissionRepo : IAdminCommissionRepo
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IEnumerable<CommissionViewModel> Retrieve()
+        {
+            var records = db.Commissions.ToList();
+            if (records == null)
+                return null;
+
+            return records;
+        }
+        public CommissionViewModel Get(int id)
+        {
+            var record = db.Commissions.Where(x => x.CommissionId == id).FirstOrDefault();
+            if (record == null)
+                return null;
+
+            return record;
+        }
+        public CommissionViewModel Create(CommissionViewModel item)
+        {
+            return null;
+        }
+        public CommissionViewModel Update(CommissionViewModel item)
+        {
+            if (item == null)
+                return null;
+
+            var itemToUpdate = db.Commissions.Where(x => x.CommissionId == item.CommissionId).FirstOrDefault();
+            itemToUpdate.DtUpdated = DateTime.UtcNow;
+            itemToUpdate.Percentage = item.Percentage;
+            db.SaveChanges();
+
+            return itemToUpdate;
+        }
+    }
+}

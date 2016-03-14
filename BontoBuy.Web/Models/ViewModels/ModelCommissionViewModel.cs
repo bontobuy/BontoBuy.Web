c1c7def3f55ac8ebb2f2df617a1ea15122c316d3
Patrick Chung <kchungsemchong@gmail.com>
2016-03-14 22:31:51 +0400

    Add ModelCommissionViewModel

---
+++ BontoBuy.Web/Models/ViewModels/ModelCommissionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuy.Web.Models
+{
+    [Table("ModelCommission")]
+    public class ModelCommissionViewModel
+    {
+        [Key]
+        [Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ModelCommissionId { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public int ModelId { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public int CommissionId { get; set; }
+
+        public ModelViewModel ModelNav { get; set; }
+        public CommissionViewModel CommissionNav { get; set; }
+    }
+}

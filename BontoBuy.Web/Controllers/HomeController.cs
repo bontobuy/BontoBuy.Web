9defc33060c50fc6f5e9e7d49165a847c59d7fdf
Patrick Chung <kchungsemchong@gmail.com>
2016-03-15 05:52:05 +0400

    Restrict only active models

--- BontoBuy.Web/Controllers/HomeController.cs
+++ BontoBuy.Web/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using BontoBuy.Web.Models;
-using Microsoft.AspNet.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Dynamic;
@@ -8,6 +6,8 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using BontoBuy.Web.Models;
+using Microsoft.AspNet.Identity;
 
 namespace BontoBuy.Web.Controllers
 {
@@ -944,7 +944,7 @@ namespace BontoBuy.Web.Controllers
 
             var modelRecords = _modelRepository.Retrieve();
             var FeaturedList = new List<HomeCatalogViewModel>();
-            foreach (var item in modelRecords.OrderBy(f => Guid.NewGuid()).Take(3))
+            foreach (var item in modelRecords.Where(x => x.Status == "Active").OrderBy(f => Guid.NewGuid()).Take(3))
             {
                 var modelItem = new HomeCatalogViewModel()
                 {
@@ -972,7 +972,7 @@ namespace BontoBuy.Web.Controllers
 
             var SlideShowList = new List<HomeCatalogViewModel>();
             Random randomNumber = new Random();
-            foreach (var item in modelRecords.OrderBy(s => randomNumber.Next(1, 20)))
+            foreach (var item in modelRecords.Where(x => x.Status == "Active").OrderBy(s => randomNumber.Next(1, 20)))
             {
                 var modelImage = new HomeCatalogViewModel()
                 {
@@ -989,7 +989,7 @@ namespace BontoBuy.Web.Controllers
             model.SlideShow = SlideShowList;
 
             var NewLaunchesList = new List<HomeCatalogViewModel>();
-            foreach (var item in modelRecords.OrderByDescending(x => x.DtCreated).Take(4))
+            foreach (var item in modelRecords.Where(x => x.Status == "Active").OrderByDescending(x => x.DtCreated).Take(4))
             {
                 var modelNewLaunch = new HomeCatalogViewModel()
                 {
@@ -1018,26 +1018,29 @@ namespace BontoBuy.Web.Controllers
             var NewLaunchesList2 = new List<HomeCatalogViewModel>();
             foreach (var item in modelRecords.OrderByDescending(x => Guid.NewGuid()).Take(4))
             {
-                var modelNewLaunch = new HomeCatalogViewModel()
+                if (item.Status == "Active")
                 {
-                    ModelId = item.ModelId,
-                    ModelNumber = item.ModelNumber,
-                    Price = item.Price,
-                    BrandName = (from m in db.Models
-                                 join b in db.Brands on m.BrandId equals b.BrandId
-                                 where m.ModelId == item.ModelId
-                                 select b.Name).FirstOrDefault(),
-                    ImageUrl = (from ph in db.Photos
-                                join pm in db.PhotoModels on ph.PhotoId equals pm.PhotoId
-                                join m in db.Models on pm.ModelId equals m.ModelId
-                                where pm.ModelId == item.ModelId
-                                select ph.ImageUrl).FirstOrDefault(),
+                    var modelNewLaunch = new HomeCatalogViewModel()
+                    {
+                        ModelId = item.ModelId,
+                        ModelNumber = item.ModelNumber,
+                        Price = item.Price,
+                        BrandName = (from m in db.Models
+                                     join b in db.Brands on m.BrandId equals b.BrandId
+                                     where m.ModelId == item.ModelId
+                                     select b.Name).FirstOrDefault(),
+                        ImageUrl = (from ph in db.Photos
+                                    join pm in db.PhotoModels on ph.PhotoId equals pm.PhotoId
+                                    join m in db.Models on pm.ModelId equals m.ModelId
+                                    where pm.ModelId == item.ModelId
+                                    select ph.ImageUrl).FirstOrDefault(),
 
-                    DtCreated = (from m in db.Models
-                                 where m.ModelId == item.ModelId
-                                 select m.DtCreated).FirstOrDefault()
-                };
-                NewLaunchesList2.Add(modelNewLaunch);
+                        DtCreated = (from m in db.Models
+                                     where m.ModelId == item.ModelId
+                                     select m.DtCreated).FirstOrDefault()
+                    };
+                    NewLaunchesList2.Add(modelNewLaunch);
+                }
             }
 
             model.NewLaunchList2 = NewLaunchesList2;

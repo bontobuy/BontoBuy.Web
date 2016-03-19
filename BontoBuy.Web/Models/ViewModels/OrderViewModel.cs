ff69d3d1aa46b40c43ca59f6dbff5067e112116c
Satish Mohun <satishmohun@gmail.com>
2016-03-20 01:43:19 +0400

    Add HasReturn & Notification property to OrderVM

--- BontoBuy.Web/Models/ViewModels/OrderViewModel.cs
+++ BontoBuy.Web/Models/ViewModels/OrderViewModel.cs
@@ -26,6 +26,8 @@ namespace BontoBuy.Web.Models
         public int Quantity { get; set; }
         public int UnitPrice { get; set; }
         public string ConfirmationCode { get; set; }
+        public bool HasReturn { get; set; }
+        public string Notification { get; set; }
         public IEnumerable<DeliveryViewModel> DeliveryNav { get; set; }
         public IEnumerable<PaymentViewModel> PaymentNav { get; set; }
         [ForeignKey("SupplierUserId")]

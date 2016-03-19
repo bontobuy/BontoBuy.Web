1d7ca36becc09c0b9e16ab623fff5bc5d8082ec2
Satish Mohun <satishmohun@gmail.com>
2016-03-20 01:35:07 +0400

    Add Notification Controller

---
+++ BontoBuy.Web/Controllers/NotificationController.cs
@@ -0,0 +1,51 @@
+﻿using BontoBuy.Web.Models;
+using Microsoft.AspNet.Identity;
+using Rotativa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BontoBuy.Web.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public void GetSupplierNotification()
+        {
+            var userId = User.Identity.GetUserId();
+            int SupplierNotification = db.Orders.Where(o => o.SupplierUserId == userId && o.Notification == "Supplier").Count();
+            ViewBag.SupplierNotification = SupplierNotification;
+        }
+
+        public void GetCustomerNotification()
+        {
+            var userId = User.Identity.GetUserId();
+            int CustomerNotification = db.Orders.Where(o => o.CustomerUserId == userId && o.Notification == "Customer").Count();
+            ViewBag.CustomerNotification = CustomerNotification;
+        }
+
+        public void GetCustomerReturnNotification()
+        {
+            var userId = User.Identity.GetUserId();
+            int CustomerNotification = (from r in db.Returns
+                                        join o in db.Orders on r.OrderId equals o.OrderId
+                                        where o.CustomerUserId == userId && r.Notification == "Customer"
+                                        select r).Count();
+            ViewBag.CustomerReturnNotification = CustomerNotification;
+        }
+
+        public void GetSupplierReturnNotification()
+        {
+            var userId = User.Identity.GetUserId();
+            int ReturnNotification = (from r in db.Returns
+                                      join o in db.Orders on r.OrderId equals o.OrderId
+                                      where o.SupplierUserId == userId && r.Notification == "Supplier"
+                                      select r).Count();
+            ViewBag.SupplierReturnNotification = ReturnNotification;
+        }
+    }
+}

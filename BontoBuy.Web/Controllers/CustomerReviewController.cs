13490d7b5188ab9032d3b6bed53c43b9ce36afdd
Satish Mohun <satishmohun@gmail.com>
2016-03-28 15:17:07 +0400

    Add review and rating For model

--- BontoBuy.Web/Controllers/CustomerReviewController.cs
+++ BontoBuy.Web/Controllers/CustomerReviewController.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using BontoBuy.Web.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using BontoBuy.Web.Models;
-using Microsoft.AspNet.Identity;
 
 namespace BontoBuy.Web.Controllers
 {
     public class CustomerReviewController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         private readonly ICustomerReviewRepo _repo;
 
         public CustomerReviewController(ICustomerReviewRepo repository)
@@ -50,6 +51,10 @@ namespace BontoBuy.Web.Controllers
                 if (User.IsInRole("Customer"))
                 {
                     int modelId = id;
+                    string modelName = (from m in db.Models
+                                        where m.ModelId == id
+                                        select m.ModelNumber).FirstOrDefault();
+                    ViewBag.ModelName = modelName;
                     bool elegible = false;
                     string userId = User.Identity.GetUserId();
 
@@ -58,14 +63,14 @@ namespace BontoBuy.Web.Controllers
 
                     elegible = _repo.VerifyReviewAbility(userId, id);
                     if (elegible == false)
-                        return RedirectToAction("Retrieve", "CustomerReview");
+                        return RedirectToAction("CustomerRetrieveOrders", "Customer", new { message = ManageMessageId.ReviewFailure });
 
                     var review = new ReviewViewModel()
                     {
                         UserId = userId,
                         ModelId = modelId
                     };
-
+                    Session["Review"] = review;
                     return View(review);
                 }
                 return RedirectToAction("Login", "Account");
@@ -78,18 +83,38 @@ namespace BontoBuy.Web.Controllers
 
         // POST: CustomerReview/Create
         [HttpPost]
-        public ActionResult Create(ReviewViewModel item)
+        public ActionResult Create(ReviewViewModel item, int rating)
         {
             try
             {
+                var reviewDetails = Session["Review"] as ReviewViewModel;
+                var test = rating;
                 if (User.IsInRole("Customer"))
                 {
-                    var newItem = _repo.Create(item);
-
                     if (item == null)
                         return RedirectToAction("Error404", "Home");
 
-                    return RedirectToAction("Retrieve", "CustomerReview");
+                    var newRecord = new ReviewViewModel
+                    {
+                        Description = item.Description,
+                        UserId = reviewDetails.UserId,
+                        ModelId = reviewDetails.ModelId
+                    };
+                    db.Reviews.Add(newRecord);
+                    db.SaveChanges();
+
+                    int ratingId = (from r in db.Ratings
+                                    where r.RatingNumber == rating
+                                    select r.RatingId).FirstOrDefault();
+                    var newRating = new RatingModelViewModel()
+                    {
+                        ModelId = reviewDetails.ModelId,
+                        RatingId = ratingId
+                    };
+                    db.RatingModels.Add(newRating);
+                    db.SaveChanges();
+                    Session.Remove("Review");
+                    return RedirectToAction("CustomerRetrieveOrders", "Customer", new { message = ManageMessageId.ReviewSuccess });
                 }
                 return RedirectToAction("Login", "Account");
             }
@@ -97,6 +122,13 @@ namespace BontoBuy.Web.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
             }
+        }
+
+        public enum ManageMessageId
+        {
+            ReviewSuccess,
+            ReviewFailure,
+            Error
         }
     }
 }

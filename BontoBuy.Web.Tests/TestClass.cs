494341940e2b83728e99d9b1a8492ab741b26b15
Satish Mohun <satishmohun@gmail.com>
2016-04-10 21:53:03 +0400

    Add Test

---
+++ BontoBuy.Web.Tests/TestClass.cs
@@ -0,0 +1,37 @@
+﻿using BontoBuy.Web.Controllers;
+using BontoBuy.Web.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BontoBuy.Web.Tests
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void RetrieveBrand()
+        {
+            //Arrange
+            var myController = new TestController();
+            List<BrandViewModel> result = null;
+            string message = String.Empty;
+
+            //Act
+            try
+            {
+                result = myController.RetrieveBrand();
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+            }
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}

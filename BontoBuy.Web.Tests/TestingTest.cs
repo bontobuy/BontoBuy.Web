b902a1395e4028e5beb8fb907613dc25bccd0749
Patrick Chung <kchungsemchong@gmail.com>
2016-04-10 12:53:26 +0400

    Add Testing

---
+++ BontoBuy.Web.Tests/TestingTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BontoBuy.Web.Controllers;
+using BontoBuy.Web.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BontoBuy.Web.Tests
+{
+    [TestClass]
+    public class TestingTest
+    {
+        [TestMethod]
+        public void RetrieveBrands()
+        {
+            //Arrange
+            var controller = new TestingController();
+            List<BrandViewModel> result = null;
+            string message = String.Empty;
+
+            //Act
+            try
+            {
+                result = controller.RetrieveBrands();
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+            }
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ BontoBuy.Web/Controllers/TestingController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BontoBuy.Web.Models;
+
+namespace BontoBuy.Web.Controllers
+{
+    public class TestingController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Testing
+        public ActionResult Retrieve()
+        {
+            return View();
+        }
+
+        public List<BrandViewModel> RetrieveBrands()
+        {
+            var records = db.Brands.ToList();
+
+            return records;
+        }
+
+        // GET: Testing/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Testing/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Testing/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Testing/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Testing/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Testing/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Testing/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

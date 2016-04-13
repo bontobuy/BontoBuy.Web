5ab0497c88d255d14d9c7fa0470e0217827a282b
Satish Mohun <satishmohun@gmail.com>
2016-04-13 21:15:53 +0400

    Add Method For Retrieve Unpaid , Paid Orders

--- BontoBuy.Web/Controllers/AdminCommissionController.cs
+++ BontoBuy.Web/Controllers/AdminCommissionController.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using BontoBuy.Web.HelperMethods;
+using BontoBuy.Web.Models;
+using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using BontoBuy.Web.Models;
 
 namespace BontoBuy.Web.Controllers
 {
-    public class AdminCommissionController : Controller
+    public class AdminCommissionController : NotificationController
     {
         private IAdminCommissionRepo _repo;
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private Helper helper = new Helper();
 
         public AdminCommissionController(IAdminCommissionRepo repository)
         {
@@ -125,7 +129,7 @@ namespace BontoBuy.Web.Controllers
             }
         }
 
-        public ActionResult RetrieveDeliveredOrders()
+        public ActionResult RetrieveDeliveredOrders(int? page, string searchString, ManageMessageId? message)
         {
             try
             {
@@ -135,7 +139,60 @@ namespace BontoBuy.Web.Controllers
                     if (records == null)
                         return RedirectToAction("Error404", "Home");
 
-                    return View(records);
+                    var recordList = new List<CommissionOrderViewModel>();
+                    foreach (var item in records)
+                    {
+                        var commission = (((from c in db.Commissions
+                                            join mc in db.ModelCommissions on c.CommissionId equals mc.CommissionId
+                                            where mc.ModelId == item.ModelId
+                                            select c.Percentage).FirstOrDefault()));
+
+                        var orderItem = new CommissionOrderViewModel()
+                        {
+                            OrderId = item.OrderId,
+                            ModelName = (from m in db.Models
+                                         where m.ModelId == item.ModelId
+                                         select m.ModelNumber).FirstOrDefault(),
+
+                            SupplierId = item.SupplierId,
+
+                            SupplierName = (from u in db.Users
+                                            where u.Id == item.SupplierUserId
+                                            select u.Name).FirstOrDefault(),
+
+                            OrderDate = item.DtCreated,
+                            OrderAmount = item.UnitPrice,
+                            Commission = Convert.ToInt32((commission * item.UnitPrice) / 100),
+                            Paid = item.CommissionPaid
+                        };
+                        if (!String.IsNullOrWhiteSpace(searchString))
+                        {
+                            var properString = helper.ConvertToTitleCase(searchString);
+                            if (orderItem.SupplierName == properString)
+                            {
+                                recordList.Add(orderItem);
+                            }
+                        }
+                        if (String.IsNullOrWhiteSpace(searchString))
+                        {
+                            recordList.Add(orderItem);
+                        }
+                    }
+
+                    //Paging Section
+                    var pageNumber = page ?? 1; // if no pagenumber is specified in the querystring, it will assign pageNumber to 1 by default
+                    var pageOfProducts = recordList.GroupBy(c => c.SupplierName).ToDictionary(g => g.Key, g => g.ToList()).ToPagedList(pageNumber, 10); //set the number of records per page
+                    ViewBag.pageOfProducts = pageOfProducts;
+
+                    ViewBag.StatusMessage =
+                           message == ManageMessageId.UpdateCommissionOrderSuccess ? "You just updated an Order Commission Status."
+                           : message == ManageMessageId.error ? "An error has occurred."
+                            : "";
+
+                    GetNewSupplierActivation();
+                    GetNewModelsActivation();
+                    ViewBag.Title = "Retrieve Unpaid Commission on Orders";
+                    return View();
                 }
 
                 return RedirectToAction("Login", "Account");
@@ -146,7 +203,186 @@ namespace BontoBuy.Web.Controllers
             }
         }
 
-        public ActionResult RetrievePaidOrders()
+        public ActionResult UpdateOrderCommission(int id)
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    GetNewSupplierActivation();
+                    GetNewModelsActivation();
+
+                    var record = db.Orders.Where(o => o.OrderId == id).FirstOrDefault();
+                    if (record == null)
+                        return RedirectToAction("RetrieveDeliveredOrders");
+
+                    var commission = (((from c in db.Commissions
+                                        join mc in db.ModelCommissions on c.CommissionId equals mc.CommissionId
+                                        where mc.ModelId == record.ModelId
+                                        select c.Percentage).FirstOrDefault()));
+
+                    var orderItem = new CommissionOrderViewModel()
+                    {
+                        OrderId = record.OrderId,
+                        ModelName = (from m in db.Models
+                                     where m.ModelId == record.ModelId
+                                     select m.ModelNumber).FirstOrDefault(),
+
+                        SupplierId = record.SupplierId,
+
+                        SupplierName = (from u in db.Users
+                                        where u.Id == record.SupplierUserId
+                                        select u.Name).FirstOrDefault(),
+
+                        OrderDate = record.DtCreated,
+                        OrderAmount = record.UnitPrice,
+                        Commission = Convert.ToInt32((commission * record.UnitPrice) / 100),
+                        Paid = record.CommissionPaid
+                    };
+
+                    return View(orderItem);
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("RetrieveDeliveredOrders");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateOrderCommission(CommissionOrderViewModel item)
+        {
+            if (User.IsInRole("Admin"))
+            {
+                var record = db.Orders.Where(o => o.OrderId == item.OrderId).FirstOrDefault();
+                if (record == null)
+                    return RedirectToAction("RetrieveDeliveredOrders", new { message = ManageMessageId.error });
+
+                record.CommissionPaid = true;
+                db.SaveChanges();
+
+                GetSupplierReturnNotification();
+                GetSupplierNotification();
+                return RedirectToAction("RetrieveDeliveredOrders", new { message = ManageMessageId.UpdateCommissionOrderSuccess });
+            }
+
+            return RedirectToAction("RetrieveDeliveredOrders", new { message = ManageMessageId.error });
+        }
+
+        public ActionResult SearchUnpaidOrders(string searchString, int? page)
+        {
+            if (!String.IsNullOrWhiteSpace(searchString))
+            {
+                var properString = helper.ConvertToTitleCase(searchString);
+                ViewBag.searchString = properString;
+
+                var record = (from o in db.Orders
+                              join m in db.Users on o.SupplierUserId equals m.Id
+                              where m.Name == properString && o.Status == "Delivered" && o.CommissionPaid == false
+                              select o).ToList();
+
+                if (record == null)
+                    return RedirectToAction("RetrieveDeliveredOrders");
+
+                var recordList = new List<CommissionOrderViewModel>();
+                foreach (var item in record)
+                {
+                    var commission = (((from c in db.Commissions
+                                        join mc in db.ModelCommissions on c.CommissionId equals mc.CommissionId
+                                        where mc.ModelId == item.ModelId
+                                        select c.Percentage).FirstOrDefault()));
+
+                    var orderItem = new CommissionOrderViewModel()
+                    {
+                        OrderId = item.OrderId,
+                        ModelName = (from m in db.Models
+                                     where m.ModelId == item.ModelId
+                                     select m.ModelNumber).FirstOrDefault(),
+
+                        SupplierId = item.SupplierId,
+
+                        SupplierName = (from u in db.Users
+                                        where u.Id == item.SupplierUserId
+                                        select u.Name).FirstOrDefault(),
+
+                        OrderDate = item.DtCreated,
+                        OrderAmount = item.UnitPrice,
+                        Commission = Convert.ToInt32((commission * item.UnitPrice) / 100),
+                        Paid = item.CommissionPaid
+                    };
+                    recordList.Add(orderItem);
+                }
+
+                //Paging Section
+                var pageNumber = page ?? 1; // if no pagenumber is specified in the querystring, it will assign pageNumber to 1 by default
+                var pageOfProducts = recordList.ToPagedList(pageNumber, 10); //set the number of records per page
+                ViewBag.pageOfProducts = pageOfProducts;
+
+                return View();
+            }
+            if (String.IsNullOrWhiteSpace(searchString))
+            {
+                return RedirectToAction("RetrieveDeliveredOrders");
+            }
+
+            return RedirectToAction("RetrieveDeliveredOrders");
+        }
+
+        public ActionResult SearchByDate(Nullable<DateTime> fromDate, Nullable<DateTime> toDate, int? page)
+        {
+            if (User.IsInRole("Admin"))
+            {
+                ViewBag.searchString = "Unpaid Commission from " + fromDate + " to " + toDate;
+
+                var record = (from o in db.Orders
+                              where o.DtCreated <= toDate && o.DtCreated >= fromDate && o.Status == "Delivered" && o.CommissionPaid == false
+                              select o).ToList();
+
+                if (record == null)
+                    return RedirectToAction("RetrieveDeliveredOrders");
+
+                var recordList = new List<CommissionOrderViewModel>();
+                foreach (var item in record)
+                {
+                    var commission = (((from c in db.Commissions
+                                        join mc in db.ModelCommissions on c.CommissionId equals mc.CommissionId
+                                        where mc.ModelId == item.ModelId
+                                        select c.Percentage).FirstOrDefault()));
+
+                    var orderItem = new CommissionOrderViewModel()
+                    {
+                        OrderId = item.OrderId,
+                        ModelName = (from m in db.Models
+                                     where m.ModelId == item.ModelId
+                                     select m.ModelNumber).FirstOrDefault(),
+
+                        SupplierId = item.SupplierId,
+
+                        SupplierName = (from u in db.Users
+                                        where u.Id == item.SupplierUserId
+                                        select u.Name).FirstOrDefault(),
+
+                        OrderDate = item.DtCreated,
+                        OrderAmount = item.UnitPrice,
+                        Commission = Convert.ToInt32((commission * item.UnitPrice) / 100),
+                        Paid = item.CommissionPaid
+                    };
+                    recordList.Add(orderItem);
+                }
+
+                //Paging Section
+                var pageNumber = page ?? 1; // if no pagenumber is specified in the querystring, it will assign pageNumber to 1 by default
+                var pageOfProducts = recordList.ToPagedList(pageNumber, 10); //set the number of records per page
+                ViewBag.pageOfProducts = pageOfProducts;
+
+                return View("SearchUnpaidOrders", recordList);
+            }
+
+            return RedirectToAction("RetrieveDeliveredOrders");
+        }
+
+        public ActionResult RetrievePaidOrders(string searchString, int? page)
         {
             try
             {
@@ -157,7 +393,56 @@ namespace BontoBuy.Web.Controllers
                     if (records == null)
                         return RedirectToAction("Error404", "Home");
 
-                    return View(records);
+                    var recordList = new List<CommissionOrderViewModel>();
+                    foreach (var item in records)
+                    {
+                        var commission = (((from c in db.Commissions
+                                            join mc in db.ModelCommissions on c.CommissionId equals mc.CommissionId
+                                            where mc.ModelId == item.ModelId
+                                            select c.Percentage).FirstOrDefault()));
+
+                        var orderItem = new CommissionOrderViewModel()
+                        {
+                            OrderId = item.OrderId,
+                            ModelName = (from m in db.Models
+                                         where m.ModelId == item.ModelId
+                                         select m.ModelNumber).FirstOrDefault(),
+
+                            SupplierId = item.SupplierId,
+
+                            SupplierName = (from u in db.Users
+                                            where u.Id == item.SupplierUserId
+                                            select u.Name).FirstOrDefault(),
+
+                            OrderDate = item.DtCreated,
+                            OrderAmount = item.UnitPrice,
+                            Commission = Convert.ToInt32((commission * item.UnitPrice) / 100),
+                            Paid = item.CommissionPaid
+                        };
+                        if (!String.IsNullOrWhiteSpace(searchString))
+                        {
+                            var properString = helper.ConvertToTitleCase(searchString);
+                            if (orderItem.SupplierName == properString)
+                            {
+                                recordList.Add(orderItem);
+                            }
+                        }
+                        if (String.IsNullOrWhiteSpace(searchString))
+                        {
+                            recordList.Add(orderItem);
+                        }
+                    }
+
+                    //Paging Section
+                    var pageNumber = page ?? 1; // if no pagenumber is specified in the querystring, it will assign pageNumber to 1 by default
+                    var pageOfProducts = recordList.GroupBy(c => c.SupplierName).ToDictionary(g => g.Key, g => g.ToList()).ToPagedList(pageNumber, 10); //set the number of records per page
+                    ViewBag.pageOfProducts = pageOfProducts;
+
+                    GetNewSupplierActivation();
+                    GetNewModelsActivation();
+
+                    ViewBag.Title = "List of Commission Paid on Orders";
+                    return View("RetrieveDeliveredOrders");
                 }
 
                 return RedirectToAction("Login", "Account");
@@ -188,6 +473,12 @@ namespace BontoBuy.Web.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
             }
+        }
+
+        public enum ManageMessageId
+        {
+            UpdateCommissionOrderSuccess,
+            error
         }
     }
 }
